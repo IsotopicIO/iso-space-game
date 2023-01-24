8deacd7d5d1922549fb3bf9ffceae9741fc7c778
Alex <alex@kaffetzakis.com>
2023-01-24 12:15:01 +0200

    Visuals parent on ship prefab.

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -1,15 +1,19 @@
 ////////////////////////////////////////////////////////////////////////////
 //
+//  (Deprecated)
+//
 //  This component will make the camera follow the ship as it moves through the scene.
 //
 //  Attach CameraController, to the main camera gameobject.              
 //
 ////////////////////////////////////////////////////////////////////////////
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Obsolete("This component is obsolete. Camera controls have been moved to ShipController.cs")]
 public class CameraController : MonoBehaviour
 {
     public ShipController ShipController;
